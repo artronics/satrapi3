01ece883659267210636f6f4d5bddfb02e07adb4
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-18 09:50:05 +0100

    add repo for sdwnNetwork

--- src/main/java/com/artronics/satrapi/services/SdwnNetworkService.java
+++ src/main/java/com/artronics/satrapi/services/SdwnNetworkService.java
@@ -1,5 +1,8 @@
 package com.artronics.satrapi.services;
 
+import com.artronics.satrapi.entities.network.SdwnNetwork;
+
 public interface SdwnNetworkService
 {
+    SdwnNetwork save(SdwnNetwork network);
 }
--- src/main/java/com/artronics/satrapi/services/impl/SdwnNetworkServiceImpl.java
+++ src/main/java/com/artronics/satrapi/services/impl/SdwnNetworkServiceImpl.java
@@ -1,9 +1,23 @@
 package com.artronics.satrapi.services.impl;
 
+import com.artronics.satrapi.entities.network.SdwnNetwork;
+import com.artronics.satrapi.repositories.SdwnNetworkRepo;
 import com.artronics.satrapi.services.SdwnNetworkService;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SdwnNetworkServiceImpl implements SdwnNetworkService
 {
     private final static Logger log = Logger.getLogger(SdwnNetworkServiceImpl.class);
+
+    @Autowired
+    private SdwnNetworkRepo networkRepo;
+
+    @Override
+    public SdwnNetwork save(SdwnNetwork network)
+    {
+        return null;
+    }
 }
