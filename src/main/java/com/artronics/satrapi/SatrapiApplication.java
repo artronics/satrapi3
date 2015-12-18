ebbe3d2cb18863842a3863d2d76a48b63ac7b4d9
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-18 07:58:03 +0100

    Init git with spring boot

---
+++ src/main/java/com/artronics/satrapi/SatrapiApplication.java
@@ -0,0 +1,12 @@
+package com.artronics.satrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatrapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SatrapiApplication.class, args);
+    }
+}
