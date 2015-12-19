c9e4b0eb2fb4be80eebb14c5b5cef6d609d20da1
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-19 07:58:29 +0100

    add test for findByNetwork and tests for eager loading network

--- src/main/java/com/artronics/satrapi/repositories/jpa/SdwnControllerRepoImpl.java
+++ src/main/java/com/artronics/satrapi/repositories/jpa/SdwnControllerRepoImpl.java
@@ -1,12 +1,12 @@
 package com.artronics.satrapi.repositories.jpa;
 
 import com.artronics.satrapi.entities.SdwnController;
+import com.artronics.satrapi.entities.SdwnNetwork;
 import com.artronics.satrapi.repositories.SdwnControllerCustomRepo;
 import com.artronics.satrapi.repositories.SdwnNetworkRepo;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -25,8 +25,16 @@ public class SdwnControllerRepoImpl implements SdwnControllerCustomRepo
     @Override
     public SdwnController findByNetwork(Long networkId, Long id)
     {
-//        SdwnNetwork network = networkRepo.findOne(networkId);
-//        SdwnController controller= em.find(SdwnController.class,id);
-        throw new NotImplementedException();
+        SdwnNetwork network = networkRepo.findOne(networkId);
+        if (network == null) {
+            return null;
+        }
+
+        SdwnController controller= em.find(SdwnController.class,id);
+
+        if (!network.getId().equals(controller.getSdwnNetwork().getId()))
+            return null;
+
+        return  controller;
     }
 }
--- src/test/java/com/artronics/satrapi/repositories/jpa/SdwnControllerRepoTest.java
+++ src/test/java/com/artronics/satrapi/repositories/jpa/SdwnControllerRepoTest.java
@@ -12,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.*;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {TestContextConfiguration.class})
@@ -25,26 +28,63 @@ public class SdwnControllerRepoTest
 
     String someIp ="12.34.64.3";
 
-    SdwnNetwork network;
-    SdwnController controller;
+    Long ctrlId;
+
+    Long netId;
+    Long otherNetId;
 
     @Before
     public void setUp() throws Exception
     {
-        network = new SdwnNetwork(someIp);
+        SdwnNetwork network = new SdwnNetwork(someIp);
         networkRepo.save(network);
+        netId = network.getId();
 
-        controller = new SdwnController();
+        SdwnController controller = new SdwnController();
         controller.setSdwnNetwork(network);
         controller.setSinkAddress(8L);
         controllerRepo.save(controller);
+        ctrlId = controller.getId();
+
+        SdwnNetwork otherNet = new SdwnNetwork("33.45.66.75");
+        networkRepo.save(otherNet);
+        otherNetId = otherNet.getId();
     }
 
     @Test
-    public void test_findByNetwork(){
-//        SdwnController persistedCnt = controllerRepo.findByNetwork(network.getId(),controller.getId());
-//
-//        assertNotNull(persistedCnt.getSdwnNetwork());
-//        assertThat(persistedCnt.getSdwnNetwork().getIp(),equalTo(someIp));
+    public void it_should_EAGER_ly_load_sdwnNetwork(){
+        SdwnController ctrl = controllerRepo.findOne(ctrlId);
+
+        assertNotNull(ctrl);
+        assertNotNull(ctrl.getSdwnNetwork());
+        assertThat(ctrl.getSdwnNetwork().getIp(),equalTo(someIp));
+    }
+
+    @Test
+    public void test_findByNetwork_it_should_return_associated_SdwnNetwork(){
+        SdwnController persistedCnt = controllerRepo.findByNetwork(netId,ctrlId);
+
+        assertNotNull(persistedCnt.getSdwnNetwork());
+        assertThat(persistedCnt.getSdwnNetwork().getIp(),equalTo(someIp));
+    }
+
+    @Test
+    public void findByNetwork_should_return_null_if_there_is_no_such_controller(){
+        SdwnController persistedCnt = controllerRepo.findByNetwork(netId,24L);
+
+        assertNull(persistedCnt);
+    }
+    @Test
+    public void findByNetwork_if_there_is_no_network_return_null(){
+        SdwnController persistedCnt = controllerRepo.findByNetwork(10L,ctrlId);
+
+        assertNull(persistedCnt);
+    }
+
+    @Test
+    public void findByNetwork_should_return_null_if_SdwnNetwork_id_does_not_match(){
+        SdwnController persistedCnt = controllerRepo.findByNetwork(otherNetId,ctrlId);
+
+        assertNull(persistedCnt);
     }
 }
