7759dcb061b3d44130dde20f582e281494a4b04b
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-24 11:15:18 +0100

    add method for adding buffer

--- senator/src/main/java/com/artronics/senator/packet/services/PacketService.java
+++ senator/src/main/java/com/artronics/senator/packet/services/PacketService.java
@@ -1,5 +1,8 @@
 package com.artronics.senator.packet.services;
 
+import java.util.List;
+
 public interface PacketService
 {
+    void addBuffer(List<Integer> buffer);
 }
--- senator/src/main/java/com/artronics/senator/packet/services/PacketServiceImpl.java
+++ senator/src/main/java/com/artronics/senator/packet/services/PacketServiceImpl.java
@@ -1,8 +1,23 @@
 package com.artronics.senator.packet.services;
 
+import com.artronics.senator.controller.device.buffer.BufferCollector;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-public class PacketServiceImpl
+import java.util.List;
+
+@Component
+public class PacketServiceImpl implements PacketService
 {
     private final static Logger log = Logger.getLogger(PacketServiceImpl.class);
+
+    @Autowired
+    BufferCollector bufferCollector;
+
+    @Override
+    public void addBuffer(List<Integer> buffer)
+    {
+
+    }
 }
