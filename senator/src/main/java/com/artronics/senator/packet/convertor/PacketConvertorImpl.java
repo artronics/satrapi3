983e8f6f2cfa378152fd60bd0ca0c6e41c83f411
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-24 11:00:13 +0100

    add impl for packet convertor

---
+++ senator/src/main/java/com/artronics/senator/packet/convertor/PacketConvertorImpl.java
@@ -0,0 +1,8 @@
+package com.artronics.senator.packet.convertor;
+
+import org.apache.log4j.Logger;
+
+public class PacketConvertorImpl
+{
+    private final static Logger log = Logger.getLogger(PacketConvertorImpl.class);
+}
