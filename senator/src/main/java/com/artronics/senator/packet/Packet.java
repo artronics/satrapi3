a727b85403273cd682eb3fbce05d01672791f94f
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-26 12:51:34 +0100

    add type and byteIndex enums to packet also some constants for def values

--- senator/src/main/java/com/artronics/senator/packet/Packet.java
+++ senator/src/main/java/com/artronics/senator/packet/Packet.java
@@ -2,5 +2,70 @@ package com.artronics.senator.packet;
 
 public interface Packet
 {
+    Integer DEF_NET_ID=1;
+    Integer DEF_MAX_TTL=20;
+
     String toLogger();
+
+    enum Type
+    {
+        DATA(0),
+        MALFORMED(1),
+        REPORT(2),
+        BEACON(3),
+        RL_REQ(128),
+        RL_RES(4),
+        OPN_PT(5);
+
+        private int value;
+
+        Type(int value)
+        {
+            this.value = value;
+        }
+
+        public int getValue()
+        {
+            return value;
+        }
+    }
+
+
+    enum Direction
+    {
+        RX,
+        TX
+    }
+    enum ByteIndex
+    {
+        LENGTH(0),
+        NET_ID(1),
+        SOURCE_H(2),
+        SOURCE_L(3),
+        DESTINATION_H(4),
+        DESTINATION_L(5),
+        TYPE(6),
+        TTL(7),
+        NEXT_HOP_H(8),
+        NEXT_HOP_L(9),
+        //        DISTANCE(10),
+        BATTERY(11),
+//        NEIGHBOUR(12),
+//        START_TIME_H(11),
+//        START_TIME_L(12),
+//        STOP_TIME_H(13),
+//        STOP_TIME_L(14),;
+        ;
+        private int value;
+
+        ByteIndex(int value)
+        {
+            this.value = value;
+        }
+
+        public int getValue()
+        {
+            return value;
+        }
+    }
 }
