ba1488013c014ef03a966d278d168f80ffe1ad8c
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-26 13:13:13 +0100

    add extra field to controll packet based on sdwn packet content

--- senator/src/main/java/com/artronics/senator/packet/ControllerPacket.java
+++ senator/src/main/java/com/artronics/senator/packet/ControllerPacket.java
@@ -1,15 +1,44 @@
 package com.artronics.senator.packet;
 
+import java.util.List;
+
 public class ControllerPacket implements Packet
 {
     private Long srcDeviceId;
     private Long srcControllerId;
+    private Direction dir;
+
+    private int len;
+    private int netId;
 
     private int srcShortAdd;
     private int dstShortAdd;
 
+    private Type type;
+
+    private int nextHop;
+
+    private ControllerPacket()
+    {
+    }
+
     public static ControllerPacket create(DevicePacket devicePacket){
-        return new ControllerPacket();
+        List<Integer> content = devicePacket.getContent();
+        ControllerPacket packet = new ControllerPacket();
+
+        packet.dir = Direction.RX;
+
+        packet.len = SdwnPacketHelper.getSize(content);
+        packet.netId = SdwnPacketHelper.getNetId(content);
+
+        packet.srcShortAdd = SdwnPacketHelper.getSourceAddress(content);
+        packet.dstShortAdd = SdwnPacketHelper.getDestinationAddress(content);
+
+        packet.type = SdwnPacketHelper.getType(content);
+
+        packet.nextHop = SdwnPacketHelper.getNextHopAddress(content);
+
+        return packet;
     }
 
     @Override
